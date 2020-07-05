4d1f231fa3177af4c6ad8c5bcc782f0f0ac13e8a
chris cnizzardini <cnizzardini@gmail.com>
2020-07-05 14:21:43 -0400

    Static analyzers, cs fixes, and support cake 4.1

--- src/Plugin.php
+++ src/Plugin.php
@@ -9,15 +9,24 @@ use Cake\Core\PluginApplicationInterface;
 
 /**
  * Class Plugin
+ *
  * @package App
  */
 class Plugin extends BasePlugin
 {
-    public function bootstrap(PluginApplicationInterface $app) : void
+    /**
+     * @param \Cake\Core\PluginApplicationInterface $app PluginApplicationInterface
+     * @return void
+     */
+    public function bootstrap(PluginApplicationInterface $app): void
     {
         parent::bootstrap($app);
     }
 
+    /**
+     * @param \Cake\Console\CommandCollection $commands CommandCollection
+     * @return \Cake\Console\CommandCollection
+     */
     public function console(CommandCollection $commands): CommandCollection
     {
         return $commands;
